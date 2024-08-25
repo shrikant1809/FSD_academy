4f9e694edc05fd752750fe1951594a27117db156
shrikant1809 <patilshrikant0918@gmail.com>
2024-08-25 19:06:39 +0530

    A

---
+++ components/layout/Topbar.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React from 'react';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { Search } from 'lucide-react';
+import { UserButton, useAuth } from '@clerk/nextjs';
+import { Button } from '../ui/button';
+
+const Topbar = () => {
+    const { isSignedIn } = useAuth();
+
+    const topRoutes = [
+        { label: "Instructor", path: "/instructor/courses" },
+        { label: "Learning", path: "/learning" },
+    ];
+
+    return (
+        <div className='flex justify-between items-center p-4'>
+            <Link href="/">
+                <Image src="/logo.png" height={100} width={200} alt="logo" />
+            </Link>
+            <div className='max-md:hidden w-[400px] rounded-full flex'>
+                <input 
+                    className='flex-grow bg-[#FF8E8B] rounded-full border-none outline-none text-dm pl-4 py-3' 
+                    placeholder='Search for courses' 
+                />
+                <button className="bg-[#FDAB04] rounded-r-full border-none outline-none cursor-pointer px-4 py-3 hover:bg-[#FDAB04]/80">
+                    <Search className="h-4 w-4" />
+                </button>
+            </div>
+            <div className="flex gap-6 items-center">
+                <div className="max-sm:hidden flex gap-6">
+                    {topRoutes.map((route) => (
+                        <Link 
+                            href={route.path}
+                            key={route.path}
+                            className="text-sm font-medium hover:text-[#FDAB04]"
+                        >
+                            {route.label}
+                        </Link>
+                    ))}
+                </div>
+                {isSignedIn ? (
+                    <UserButton afterSignOutUrl="/sign-in" />
+                ) : (
+                    <Link href="/sign-in">
+                        <Button>
+                        Sign In
+                        </Button>
+                    </Link>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default Topbar;
